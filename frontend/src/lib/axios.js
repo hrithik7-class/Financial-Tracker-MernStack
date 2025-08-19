11bdbb5ff28e612917b013629d38e7c1bd57b5bb
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-19 16:50:04 +0530

    file changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getBaseURL = () =>{
     if(import.meta.env.Prod){
-        return import.meta.env.VITE_API_URL ;
+        return import.meta.env.VITE_API_URL || "https://financial-tracker-mernstack.onrender.com";
     }
     return import.meta.env.VITE_API_URL;
 }
@@ -11,3 +11,4 @@ const instance = axios.create({
     withCredentials: true,
 })
 export default instance;
+// VITE_API_URL=http://localhost:5000 .env.developer  //  VITE_API_URL=https://financial-tracker-mernstack.onrender.com  production
