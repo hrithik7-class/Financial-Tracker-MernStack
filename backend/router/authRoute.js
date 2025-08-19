dbc48e427ac77412b515c34c719ff0496ab1bfca
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-19 16:32:30 +0530

    changes in routes

--- backend/router/authRoute.js
+++ backend/router/authRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getProfile, login, logout, signup } from '../controllers/authController.js';
+import { getProfile, login, logout, signup , refreshAccessToken } from '../controllers/authController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router()
 
-router.post("/login", login)
-router.post("/signup", signup)
-router.post('/logout', authMiddleware,logout)
-router.get('/profile', authMiddleware, getProfile)
+router.route("/login").post(login)
+router.route("/signup").post(signup)
+router.route("/logout").post(authMiddleware,logout)
+router.route("/profile").get(authMiddleware,getProfile)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
--- backend/router/financeRoute.js
+++ backend/router/financeRoute.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { getAllTranscation, addIncome, deleteTranscation, updateTranscation  } from '../controllers/financeController.js'
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', authMiddleware, getAllTranscation)
-router.post('/', authMiddleware, addIncome)
-router.delete('/:id', authMiddleware, deleteTranscation)
-router.put('/:id', authMiddleware, updateTranscation)
+router.route("/").get(authMiddleware, getAllTranscation);
+router.route("/").post(authMiddleware, addIncome);
+router.route("/:id").delete(authMiddleware, deleteTranscation);
+router.route("/:id").put(authMiddleware, updateTranscation);
 
 export default router;
 
