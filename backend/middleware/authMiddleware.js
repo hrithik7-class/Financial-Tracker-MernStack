0f38d0ade49e30f02e8e5f0e843271d60c1c177d
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-19 20:31:18 +0530

    console remove from the authmiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -19,7 +19,7 @@ export const authMiddleware = async (req, res, next) => {
         next();
 
     } catch (error) {
-        console.log("Invalid token");
+        
         return res.status(401).json({ message: 'Invalid token' });
     }
 }
