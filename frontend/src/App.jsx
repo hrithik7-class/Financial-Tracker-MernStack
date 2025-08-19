c38724d77060dfcc1c21686537fd1642863b19d2
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-19 20:28:33 +0530

    loading problem slove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route , Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { useEffect } from "react"
-import { useSelector , useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { Toaster } from "react-hot-toast"
 
 import { Dashboard } from "./pages/Dashboard"
@@ -9,43 +9,50 @@ import { SignUpPage } from "./pages/SignupPage"
 import { Loading } from "./components/Loading"
 import NotFound from "./pages/NotFound"
 
-import { fetchProfile } from './feature//auth/authSlice';
+import { fetchProfile } from './feature/auth/authSlice';
 import { fetchTransactions } from './feature/finance/finance.Slice';
 
-
 function App() {
+  const user = useSelector(state => state.auth.user);
+  const isInitialized = useSelector(state => state.auth.isInitialized);
+  const dispatch = useDispatch();
 
-  const { user,loading ,isAuthenticated } = useSelector(state => state.auth);
-  const dispatch = useDispatch() 
-   
-    useEffect(() => {
-    dispatch(fetchProfile()).then(() => {
-      dispatch(fetchTransactions());
-    });
-  }, [dispatch]);
+  useEffect(() => {
+    if (!isInitialized) {
+      (async () => {
+        const profileAction = await dispatch(fetchProfile());
+        if (fetchProfile.fulfilled.match(profileAction)) {
+          dispatch(fetchTransactions());
+        }
+      })();
+    }
+  }, [dispatch, isInitialized]);
 
-  if (loading && !user && isAuthenticated ) return <Loading />;
-  
+  // Show loading until auth check is complete
+  if (!isInitialized) {
+    return <Loading />;
+  }
+
   return (
-    <> 
-      <div className=" min-h-screen text-white bg-gray-900 relative overflow-hidden">
+    <>
+      <div className="min-h-screen text-white bg-gray-900 relative overflow-hidden">
         <div className='absolute inset-0 overflow-hidden'>
           <div className='absolute inset-0'>
-            <div className='absolute top-0 left-1/2  -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]'
-            />
+            <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
           </div>
-        </div>    
-        <div className='z-50  relative'>     
+        </div>
+        <div className='z-50 relative'>
           <Routes>
-            <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-            <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to="/" />} />
-            <Route path="/signup" element={ <SignUpPage />}  />
-            <Route path="*" element={<NotFound/>} />
+            <Route path="/" element={user ? <Dashboard/> : <Navigate to="/login" />} />
+            <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+            <Route path="/signup" element={user ? <Navigate to="/" /> : <SignUpPage />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-        <Toaster/>
+        <Toaster />
       </div>
     </>
   )
 }
-export default App
+
+export default App;
